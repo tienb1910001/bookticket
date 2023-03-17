476f0dda9e304d850a80354c389b766e5741ed1b
tienb1910001 <tienb1910001@student.ctu.edu.vn>
2023-03-17 15:16:16 +0700

    booktickets app

---
+++ lib/notfoundscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotFoundScreen extends StatelessWidget {
+  const NotFoundScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,39 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.png',
+    'place': 'One Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'two.png',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'three.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  }
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': 'NYC', 'name': 'New-York'},
+    'to': {'code': 'LDN', 'name': 'London'},
+    'flying_time':'8H 30M',
+    'date':'1 MAY',
+    'departure_time':'80:00 AM',
+    'number':23,
+  },
+  {
+    'from': {'code': 'DK', 'name': 'Dhaka'},
+    'to': {'code': 'SH', 'name': 'Shanghai'},
+    'flying_time':'4H 20M',
+    'date':'3 MAY',
+    'departure_time':'90:00 AM',
+    'number':45,
+  },
+];
